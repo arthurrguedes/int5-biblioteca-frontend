f50cffb9ca51c2d240a7f99c5fbcc8f2fc90f7b0
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-09-25 21:14:01 -0300

    não finalizado

--- biblioteca-plus/src/app/pages/login-usuario/login-usuario.component.ts
+++ biblioteca-plus/src/app/pages/login-usuario/login-usuario.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // <--- importar aqui
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../shared/auth/auth.service';
 
 @Component({
   selector: 'app-login-usuario',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './login-usuario.component.html',
   styleUrls: ['./login-usuario.component.scss']
 })
-export class LoginUsuarioComponent {
 
+export class LoginUsuarioComponent {
+  email = '';
+  senha = '';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  entrar() {
+     // ADICIONE ESTAS DUAS LINHAS PARA DEBUGAR
+    console.log('Valor do Email no componente:', this.email);
+    console.log('Valor da Senha no componente:', this.senha);
+    // aqui você faria a validação de email/senha de verdade com o backend
+    if (this.email === 'user@login.com' && this.senha === '12345') {
+      this.auth.loginAsUser();    // seta role user
+      this.router.navigate(['/']);
+    } else {
+      alert('Credenciais inválidas');
+    }
+  }
 }
