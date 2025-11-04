a5273c187c79a1e56f10e4898696a538eaa367cb
Biel29Souza <biel29souza98@gmail.com>
2025-11-03 21:58:09 -0300

    Ajustando o Hero

--- biblioteca-plus/src/app/shared/hero/hero.component.ts
+++ biblioteca-plus/src/app/shared/hero/hero.component.ts
@@ -3,33 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   standalone: true,
-
-  // Utilizando a imagem como hero
-  // styles: [`
-  //   .hero {
-  //     background-image: url('/assets/hero1.PNG');
-  //     background-size: cover;
-  //     background-position: center;
-  //     background-repeat: no-repeat;
-  //     padding: 120px 0;
-  //     color: white;
-  //   }
-  // `],
+  styles: [`
+    .hero img {
+      width: 100%;
+      height: auto; /* mantém proporção natural */
+      display: block;
+      object-fit: cover;
+    }
+  `],
   template: `
-  <section class="hero">
-    <div class="container">
-      <div class="row align-items-center g-4">
-        <div class="col-lg-7">
-          <h1 class="display-5 mb-3">Esta é a <br/> Biblioteca+</h1>
-        </div>
-        <div class="col-lg-5">
-          <p class="tagline fs-5">
-            O site onde você encontra seus livros favoritos!
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
+    <section class="hero">
+      <img src="/assets/hero1.PNG" alt="Hero image">
+    </section>
   `
 })
 export class HeroComponent {}
